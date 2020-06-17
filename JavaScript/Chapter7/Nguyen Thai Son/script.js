bd1ccab7f215ef1649fe32eba86a018deb7297df
sonnguyen92 <64121583+sonnguyen92@users.noreply.github.com>
2020-06-17 19:30:41 +0700

    commit

--- JavaScript/Chapter7/Nguyen Thai Son/script.js
+++ JavaScript/Chapter7/Nguyen Thai Son/script.js
@@ -5,40 +5,41 @@ var $ = function(id) {
 var check = function () {
     var inputNum = $('random');
     inputNum = $('random').value;
-    
+    var arr = [];
+
+    var delDiv = document.getElementsByClassName('opt col-lg-6 mt-2');
+    if (delDiv.length > 0) {
+        delDiv[0].firstChild.remove();
+    }
+
     if (inputNum == "") {
         alert('Pls enter a value.');
     } else if (inputNum < 1) {
         alert('Value must be > 1');
     } else {
-        for (let i=1; i <= inputNum; i++) {
-            var randomResult = Math.ceil(Math.random()*100);
-            var createDiv = document.createElement('div');
+        var createDiv = document.createElement('div');
+        for (let i=0; i < inputNum; i++) {
+            var removeDiv = document
+            
+            arr[i] = Math.ceil(Math.random()*100);
+            
             var createP = document.createElement('p');
-            var createText = document.createTextNode('Số thứ ' + i + ' là: ' + randomResult);
+            var createText = document.createTextNode('Số thứ ' + (i+1) + ' là: ' + arr[i]);
             createP.appendChild(createText);
             createDiv.appendChild(createP);
-            $('outputDiv').appendChild(createDiv);
-
-            var arr = [];
-            arr[i] = randomResult;
-            var max = Math.max(arr);
-            var min = Math.min(arr);
-            var createDivMinMax = document.createElement('div');
-            var createPMax = document.createElement('p');
-            var createPMin = document.createElement('p');
-            var createTextMax = document.createTextNode('Số lớn nhất là: ')
         }
-    }
-
-    /*for (let i=1; i <= inputNum; i++) {
-        var randomResult = Math.ceil(Math.random()*100);
-        var createDiv = document.createElement('div');
-        var createP = document.createElement('p');
-        var createText = document.createTextNode('Số thứ ' + i + ' là: ' + randomResult);
-        createP.appendChild(createText);
-        createDiv.appendChild(createP);
+        var max = Math.max(...arr);
+        var min = Math.min(...arr);
+        var createDivMinMax = document.createElement('div');
+        var createPMax = document.createElement('p');
+        var createPMin = document.createElement('p');
+        var createTextMax = document.createTextNode('Số lớn nhất là: ' + max);
+        var createTextMin = document.createTextNode('Số nhỏ nhất là: ' + min);
+        createPMax.appendChild(createTextMax);
+        createPMin.appendChild(createTextMin);
+        createDiv.appendChild(createPMax);
+        createDiv.appendChild(createPMin);
         $('outputDiv').appendChild(createDiv);
-    }*/
+    }
 }
 $('checkBtn').onclick = check;
