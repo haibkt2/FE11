2c363605f734966b71c758616d70bfc7d050b8e0
haibkt2 <haibkt2@gmail.com>
2020-06-19 18:53:42 +0700

    Update calculate

--- JavaScript/Caculate/caculate.js
+++ JavaScript/Caculate/caculate.js
@@ -2,10 +2,62 @@ function $(id) {
 	return document.getElementById(id);
 }
 function calculate() {
-	// validate for subtotal
-	valid(v, Math.max);
-	// validate for amount
-	valid(v, 400);
+	var t, a, p, c = true;
+	// Get value input
+	var s = $("id_subtotal").value;
+	// Validate
+	if(!valid(s, Math.max)) {
+		// Calculate
+		s = parseFloat(s);
+		if(s < 10000) {
+			a = 0;
+			p = 0.1;
+		} else {
+			var v_select = $("opt_select").value;
+			switch (v_select) {
+				case 0:
+					a = 200;
+					p = (s < 15000) ? 0.1 : 0.15;
+					break;
+				case 1:
+					a = 300;
+					p = (s < 15000) ? 0.15 : 0.2;
+					break;
+				case 2:
+					a = $("id_amount").value;  
+					if(valid(a, 500)){
+						c = false;
+					} else {
+						a = parseFloat(a);
+						p = 0.15;
+					}
+					break;
+				default:
+					displayError();
+			}
+		}
+		// calculate
+		if(c) {
+			t = s * p - a;
+			// Display value
+			displayValue(p, a, t );
+		} else {
+			displayError();
+		}
+	} else {
+		displayError();
+	}
+	
+}
+function displayValue(p, a, t) {
+	$("id_percent").value = p;
+	$("id_amount").value = a;
+	$("id_total").value = t;
+}
+function displayError() {
+	alert("Pls input again");
+	$("id_form").reset();
+	$("id_subtotal").focus(); 
 }
 function getAmount() {
 	var v_input = $("id_subtotal").value;
@@ -16,13 +68,8 @@ function getAmount() {
 		$("id_amount").disabled = true;
 	}
 }
-function valid(value, condition) {
-	var checked = (isNaN(value) || value < 0 || value > condition) ? true : false;
-	if(checked) {
-		alert("Pls input again");
-		$("id_form").reset();
-		$("id_subtotal").focus();
-	}
+function valid(value, max) {
+	var checked = (isNaN(value) || value < 0 || value >= max) ? true : false;
 	return checked;
 }
 $("bt_calculate").onclick = calculate;
