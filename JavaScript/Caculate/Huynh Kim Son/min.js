75afaeaea36f90f34f65e0d1724829cad2011659
171121521130 <59369086+171121521130@users.noreply.github.com>
2020-06-19 18:59:33 +0700

    Update min.js

--- JavaScript/Caculate/Huynh Kim Son/min.js
+++ JavaScript/Caculate/Huynh Kim Son/min.js
@@ -1,6 +1,70 @@
 function $(id){
     return document.getElementById(id);
 }
+
+function calculate(){
+     //validate for subtotal
+     var total, amount, precent, c=true;
+     // get value input
+     var subtotal = $("id_subtotal").value;
+     // validate
+     if(!valid(subtotal,Math.max)){
+         //caludate
+         subtotal = parseFloat(subtotal);
+         if(subtotal < 10000){
+           amount = 0 ;
+           precent = 0.1;
+        } else {
+               var op_select = $("id_select").value;
+               switch (op_select){
+                    case 0:
+                       amount = 200;
+                       precent = ( subtotal < 15000) ? 0.1 : 0.15;
+                       break;
+                    case 1:
+                        amount = 300;
+                        precent = (subtotal < 15000) ? 0.15 : 0.2;
+                        break;
+                    case 2:
+                        amount = $("id_amount").value;
+                        if(valid(amount, 500)){
+                            c = false;
+                        } else{
+                            amount = parseFloat(amount);
+                            precent = 0.15;
+                        }
+                        break;
+                    default:
+                        displayError();
+                      
+               }
+           }
+        }
+        // calculate
+        if(c){
+            total = subtotal * precent - amount;
+            //display values
+            displayValue(percent, amount, total);
+
+        } else {
+            displayError();
+            
+        } 
+  }
+
+//
+function displayValue(percent, amount, total) {
+	$("id_percent").value = precent;
+	$("id_amount").value = amount;
+	$("id_total").value = total;
+}
+// displayError
+function displayError(){
+    alert("Pls, input again.");
+    $("id_form").reset();
+    $("id_subtotal").focus();
+}
+
 // getamount
 function getAmount() {
 	var input_sub = $("id_subtotal").value;
@@ -11,63 +75,15 @@ function getAmount() {
 		$("id_amount").disabled = true;
 	}
 }
-// valid
-function valid(value, condition){
-    var checked = (isNaN(value) || value < 0 || value > condition ) ? true : false;
-    if(checked){
-        alert("Pls, input again.");
-        $("id_form").reset();
-        $("id_subtotal").focus();
-    }
+
+// valid thuc hien tra ve true or false
+function valid(value, max){
+    var checked = (isNaN(value) || value < 0 || value >= max ) ? true : false;
     return checked;
-}
-function calculate(){
-    //validate for subtotal
-    var input_sub = ("id_subtotal").value;
-    var op_select = ("id_select").value;
-    // validate for subtotal
-	//valid(v, Math.max);
-	// validate for amount
-	//valid(v, 400);
-    // set subtotal <10000
-    if(input_sub<10000)
-    {
-        $("id_amount").setAttribute("value","0");
-        $("id_percent").setAttribute("value","10");
-        
-    }
-    // set loyalty
-    else if(op_select== 0){
-        $("id_amount").setAttribute("value","200");
-        $("id_percent").setAttribute("value","10");
-        if(input_sub < 15000){
-            $("id_percent").setAttribute("value","15");
-        }
-    }
-    //set honored
-    else if(op_select== 1){
-        $("id_amount").setAttribute("value","300");
-        $("id_percent").setAttribute("value","15");
-        if(input_sub < 15000){
-            $("id_percent").setAttribute("value","20");
-        }
-    }
-    //other
-    else if(op_select== 2){
-        $("id_percent").setAttribute("value","15");
-    }
-    // ép kiểu trả về giá trị số number
-    var input_sub = parseFloat($("id_subtotal").value);
-    var op_select = parseFloat($("id_select").value);
-    var total = parseFloat($("id_total").value);
-    var precent = parseFloa($("id_percent").value);
-    var amount = parseFloat($("id_amount").value);
-    // công thức tính total
-   total = ((input_sub *(precent/100))-(amount))
-   $("id_total").value=total;
 }
 
 $("bt_calculate").onclick = calculate;
 $("id_select").onchange = getAmount;
 $("id_subtotal").onchange = getAmount;
+
 
