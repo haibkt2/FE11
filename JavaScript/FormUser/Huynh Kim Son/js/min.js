e86048fad744165b9b0923ceb576e203c716b21a
171121521130 <59369086+171121521130@users.noreply.github.com>
2020-07-01 15:37:46 +0700

    update

--- JavaScript/FormUser/Huynh Kim Son/js/min.js
+++ JavaScript/FormUser/Huynh Kim Son/js/min.js
@@ -10,12 +10,12 @@ function regUser() {
 	}
 }
 function removeErro () {
-	// body... 
 	var cl_error = document.getElementsByClassName('error');
 	for(let i = 0 ; i < cl_error.length;i++){
 		cl_error[i].remove();
 	}
 }
+
 function validate(){
 	removeErro();
 	var arr_input = document.getElementsByClassName('cl-user');
@@ -23,55 +23,105 @@ function validate(){
 		var v_input = arr_input[i].value.trim();
 		// check required
 		if(v_input == '') {
-			arr_input[i].parentElement.innerHTML += '<p class ="error">Error</p>';
+			arr_input[i].parentElement.innerHTML += '<p class ="error">Please enter information.</p>';
 			return false;
 		}
 		// check pass
 		var attr = arr_input[i].getAttribute('type');
 		if(attr == 'password') {
 			if(v_input.length < 8) {
-				arr_input[i].parentElement.innerHTML += '<p class ="error"> Password > 8</p>';
+				arr_input[i].parentElement.innerHTML += '<p class ="error"> Password is more than 8 characters.</p>';
 				return false;
 			}
 			else {
-				if(v_input != $('re-pass').value)
-					arr_input[i].parentElement.innerHTML += '<p class ="error">not same with pass</p>';
+				if(v_input != $('re-pass').value){
+					arr_input[i].parentElement.innerHTML += '<p class ="error">Not same with pass.</p>';
 					return false;
+
+					}
 				}
 		}
+		//check mail
+		if (attr == "email") {
+				var atposition = v_input.indexOf("@");
+        		var dotposition = v_input.lastIndexOf(".");
+        		if (atposition < 1 || dotposition < (atposition + 2) || (dotposition + 2) >= v_input.length) {
+					arr_input[i].parentElement.innerHTML += '<p class ="error">Please enter a valid e-mail address.</p>';
+           			 return false;
+       		 }
+		}
+		//check phone
+		// if (attr == "phone") {
+		// 	var rule_phone = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+		// 		if (rule_phone.test(v_input) == false) 
+		// 		{
+		// 			arr_input[i].parentElement.innerHTML += '<p class ="error">Số điện thoại của bạn không đúng định dạng!</p>';
+
+		// 		}
+		// }
 	}	
 	return true;
 }
-var user_obj = function(user_name, pass, full_name, email, phone) {
+var user_obj = function(user_name, pass, last_name, first_name, email, phone) {
 	this.user_name = user_name;
 	this.pass = pass;
-	this.full_name = full_name;
+	this.last_name = last_name;
+	this.first_name = first_name;
 	this.email = email;
 	this.phone = phone;
 }
 function registerUser() {
-	// body... 
 	// check user name
 	var user_name_input = $('user_name').value;
 	var user_name_reg = localStorage.getItem(user_name_input);
 	if(user_name_reg == null) {
-		var user = new user_obj(user_name_input, $('pass').value,
-				$('full_name').value, $('email').value, $('phone').value);
-		var info = user.user_name + ':' + user.pass + ':' + user.full_name 
-								+ ':' + user.email	+ ':' + user.phone;
+		var user = new user_obj(user_name_input, 
+								$('pass').value,
+								$('last_name').value,
+								$('first_name').value, 
+								$('email').value, 
+								$('phone').value);
+		var info = user.user_name + ':' + user.pass 
+								  + ':' + user.last_name 
+								  + ':' + user.first_name 
+								  + ':' + user.email	
+								  + ':' + user.phone;
 		localStorage.setItem(user_name_input, info);
+		alert('Đăng ký thành công !')
+		location.replace("../html/Login.html");
+
 	} else {
-		alert("Error !")
+		alert('Tên đăng nhập đang được sử dụng. Xin vui lòng dùng tên đăng nhập khác !')
+
 	}
 }
+
+// js login
 function login() {
-	var v_user_name = $('user_name').value;
-	var info_use = localStorage.getItem(v_user_name);
-	if (info_use == null) {
-		alert("Error !!")
-	} else {
-		var v_pass = $('pass').value;
-		var pass_reg = info_use.split(':')[1];
+	removeErro();
+	var user_name_input = $('user_name').value;
+	var pass_input = $('pass').value;
+	var info_use = localStorage.getItem(user_name_input);
+	info_use = JSON.parse(info_use);
+	if (user_name_input == '' || info_use  == null) {
+		$('user_name').parentElement.innerHTML += '<p class ="error">Tên đăng nhập không đúng !</p>';
 	}
-}
+	if (pass_input == '' || info_use == null) {
+		$('pass').parentElement.innerHTML += '<p class ="error"> Mật khẩu không đúng !</p>';
+		return false;
+
+	}
+	var user_login = info_use.user_name;
+	var pass_login = info_use.pass;
+		if (user_login == user_name_input && pass_login == pass_input) {
+			alert(" Chúc mừng bạn đã đăng nhập thành công !");
+
+		}else {
+			alert(" Pls, vui lòng nhập đúng thông tin !");
+			return false;
+			
+		}
+	}
+
+$('bt_login').onclick = login;
 $('bt_register').onclick = regUser;
