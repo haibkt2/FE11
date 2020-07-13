3b0f57deb734744371f0b484a1fdaaf2895b4920
171121521130 <59369086+171121521130@users.noreply.github.com>
2020-07-13 18:03:52 +0700

    update

--- jQuery/Session 5/Huỳnh Kim Sơn/js/animate.js
+++ jQuery/Session 5/Huỳnh Kim Sơn/js/animate.js
@@ -6,25 +6,25 @@ $(document).ready(function(){
         $(".child").animate({left:'0px'},1000);
         $(".child").animate({top:'50px'},1000);
 
-        $(".child").animate({left:'400px'},1000);
-        $(".child").animate({top:'400px'},1000);
-        $(".child").animate({left:'50px'},1000);
-        $(".child").animate({top:'100px'},1000);
+        $(".child").animate({left:'400px'},900);
+        $(".child").animate({top:'400px'},900);
+        $(".child").animate({left:'50px'},900);
+        $(".child").animate({top:'100px'},900);
 
-        $(".child").animate({left:'350px'},1000);
-        $(".child").animate({top:'350px'},1000);
-        $(".child").animate({left:'100px'},1000);
-        $(".child").animate({top:'150px'},1000);
+        $(".child").animate({left:'350px'},800);
+        $(".child").animate({top:'350px'},800);
+        $(".child").animate({left:'100px'},800);
+        $(".child").animate({top:'150px'},800);
 
-        $(".child").animate({left:'300px'},1000);
-        $(".child").animate({top:'300px'},1000);
-        $(".child").animate({left:'150px'},1000);
-        $(".child").animate({top:'200px'},1000);
+        $(".child").animate({left:'300px'},700);
+        $(".child").animate({top:'300px'},700);
+        $(".child").animate({left:'150px'},700);
+        $(".child").animate({top:'200px'},700);
 
-        $(".child").animate({left:'250px'},700);
-        $(".child").animate({top:'250px'},700);
-        $(".child").animate({left:'200px'},700);
-        $(".child").animate({top:'250px'},700);
+        $(".child").animate({left:'250px'},600);
+        $(".child").animate({top:'250px'},600);
+        $(".child").animate({left:'200px'},600);
+        $(".child").animate({top:'250px'},600);
 
         $(".child").animate({top:'225px'},500);
         $(".child").animate({left:'225px'},500);
