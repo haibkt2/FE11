fe879ed7e331e304ec19572c55074549cf6682f2
Hoai Pham <64089642+dinhhoai2103@users.noreply.github.com>
2020-07-13 18:28:29 +0700

    Update index.js

--- jQuery/Animation/Phạm Đình Hoài/index.js
+++ jQuery/Animation/Phạm Đình Hoài/index.js
@@ -5,6 +5,6 @@
            i += 50;
            j += 50;
            $("#div").animate({ "top":650-i ,backgroundColor: 'blue' }, 1000 ).animate({ "left": 650-j, backgroundColor: 'yellow' }, 1000 ).
-           animate({ "top": i, backgroundColor: 'purple' }, 1000 ).animate({ "left": j, backgroundColor: 'red' }, 1000 ); 
+           animate({ "top": i-50, backgroundColor: 'purple' }, 1000 ).animate({ "left": j, backgroundColor: 'red' }, 1000 ); 
         } while (( i !== 300 ) && ( j !== 300));
         },1000)
