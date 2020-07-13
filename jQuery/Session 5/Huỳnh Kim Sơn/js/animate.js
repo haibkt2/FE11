5e7cef67051b13290e9d551b21ba50b9d1cadecd
171121521130 <59369086+171121521130@users.noreply.github.com>
2020-07-13 20:19:39 +0700

    update

--- jQuery/Session 5/Huỳnh Kim Sơn/js/animate.js
+++ jQuery/Session 5/Huỳnh Kim Sơn/js/animate.js
@@ -1,33 +1,36 @@
-
 $(document).ready(function(){
     $("#id_start").click(function(){
-        $(".child").animate({left:'450px'},1000);
-        $(".child").animate({top:'450px'},1000);
-        $(".child").animate({left:'0px'},1000);
-        $(".child").animate({top:'50px'},1000);
+        $("#child").animate({left:'450px'},1000);
+        $("#child").animate({top:'450px'},1000);
+        $("#child").animate({left:'0px'},1000);
+        $("#child").animate({top:'50px'},1000);
 
-        $(".child").animate({left:'400px'},900);
-        $(".child").animate({top:'400px'},900);
-        $(".child").animate({left:'50px'},900);
-        $(".child").animate({top:'100px'},900);
+        $("#child").animate({backgroundColor: "yellow",left:'400px'},900);
+        $("#child").animate({top:'400px'},900);
+        $("#child").animate({left:'50px'},900);
+        $("#child").animate({top:'100px'},900);
 
-        $(".child").animate({left:'350px'},800);
-        $(".child").animate({top:'350px'},800);
-        $(".child").animate({left:'100px'},800);
-        $(".child").animate({top:'150px'},800);
+        $("#child").animate({backgroundColor: "#aa0000",left:'350px'},800);
+        $("#child").animate({top:'350px'},800);
+        $("#child").animate({left:'100px'},800);
+        $("#child").animate({top:'150px'},800);
 
-        $(".child").animate({left:'300px'},700);
-        $(".child").animate({top:'300px'},700);
-        $(".child").animate({left:'150px'},700);
-        $(".child").animate({top:'200px'},700);
+        $("#child").animate({backgroundColor: "	#00ff80",left:'300px'},700);
+        $("#child").animate({top:'300px'},700);
+        $("#child").animate({left:'150px'},700);
+        $("#child").animate({top:'200px'},700);
 
-        $(".child").animate({left:'250px'},600);
-        $(".child").animate({top:'250px'},600);
-        $(".child").animate({left:'200px'},600);
-        $(".child").animate({top:'250px'},600);
+        $("#child").animate({backgroundColor: "	#00ffff",left:'250px'},600);
+        $("#child").animate({top:'250px'},600);
+        $("#child").animate({left:'200px'},600);
+        $("#child").animate({top:'250px'},600);
 
-        $(".child").animate({top:'225px'},500);
-        $(".child").animate({left:'225px'},500);
+        $("#child").animate({backgroundColor: "white",top:'225px'},500);
+        $("#child").animate({left:'225px'},500);
 
     });
+});
+
+$("#id_stop").click(function(){
+    $("#child").stop(true);
 });
---
+++ jQuery/Session 5/Huỳnh Kim Sơn/js/demo.js
@@ -0,0 +1,14 @@
+//slide
+$('.bxslider').bxSlider({
+    auto: true,
+    autoControls: true,
+    stopAutoOnClick: true,
+    pager: true,
+    slideWidth: 1200,
+  });
+
+  $( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  } );
